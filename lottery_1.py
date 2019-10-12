14a9001802f06e4fe61fc79b975d1f6010710fc8
Kino Mondesir <kino.mondesir@gmail.com>
2019-10-12 09:36:02 -0400

    updated the .gitignore

--- lottery_1.py
+++ lottery_1.py
@@ -3,6 +3,29 @@ import os
 
 from lottery import winnings as wins
 
+import logging as log
+severity = {
+            'CRITICAL': 50,
+            'ERROR': 40,
+            'WARNING': 30,
+            'INFO': 20,
+            'DEBUG': 10,
+            'NOTSET': 0,
+}
+
+logger = log.getLogger(__name__)
+formatter = log.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler = log.FileHandler("lottery_1.py")
+file_handler.setLevel(severity["INFO"])
+file_handler.setFormatter(formatter)
+
+stream_handler = log.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+
 path = os.getcwd()
 print(path)
 item = 'Lottery_Take_5_Winning_Numbers.csv'
