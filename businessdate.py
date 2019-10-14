9246fcd83c4512e3a3ee8907f80bfa80e1729e9e
Kino Mondesir <31377409+kmondesir@users.noreply.github.com>
2019-10-14 11:42:55 -0400

    Moved the weekdays list into the class
    Moving the weekdays list into the class to change its scope to being a class variable instead of a module variable. This is for better organization keeping variables and functions together logically.

--- businessdate.py
+++ businessdate.py
@@ -3,10 +3,8 @@
 
 
 import logging as log
+import calendar as cal
 from datetime import date, timedelta
-
-weekdays = ["Monday", "Tuesday", "Wednesday",
-            "Thursday", "Friday", "Saturday", "Sunday"]
 
 severity = {
   'CRITICAL':50,
@@ -32,6 +30,9 @@ logger.addHandler(stream_handler)
 
 class returndate:
   """ Returns calculated date by adding or subtracting days """
+	
+	weekdays = ["Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Saturday", "Sunday"]
 
   minus1 = -1
   minus2 = -2
@@ -58,12 +59,12 @@ class returndate:
   def previousbusinessdate(self):
     """ Returns previous business date, skipping the weekend"""
     
-    if self.r_date.weekday() == weekdays.index('Saturday'):
+    if self.r_date.weekday() == returndate.weekdays.index('Saturday'):
       # subtract 1 day
       result = (self.r_date + timedelta(days=returndate.minus1)).isoformat()
       logger.debug(result)
       return result
-    elif self.r_date.weekday() == weekdays.index('Sunday'):
+    elif self.r_date.weekday() == eturndate.weekdays.index('Sunday'):
       # subtract 2 days
       result = (self.r_date + timedelta(days=returndate.minus2)).isoformat()
       logger.debug(result)
@@ -77,12 +78,12 @@ class returndate:
   def nextbusinessdate(self):
     """ Returns next business date, skipping the weekend"""
 
-    if self.r_date.weekday() == weekdays.index('Saturday') :
+    if self.r_date.weekday() == returndate.weekdays.index('Saturday') :
       # add 1 day
       result = (self.r_date.weekday() + timedelta(days=returndate.plus2)).isoformat()
       logger.debug(result)
       return result
-    elif self.r_date.weekday() == weekdays.index('Sunday'):
+    elif self.r_date.weekday() == returndate.weekdays.index('Sunday'):
       # add 2 days
       result = (self.r_date.weekday() + timedelta(days=returndate.plus1)).isoformat()
       logger.debug(result)
@@ -94,8 +95,29 @@ class returndate:
       return result
 
   def weekday(self):
-    # returns a string representation of the weekday
+    # returns a string representation of the weekday 
     return weekdays[self.r_date.weekday()]
 
+  def firstdateofweek(self):
+		#returns a the first date of a given week determined by the date attribute passed in
+		length = len(returndate.weekdays)
+		numberofdaysfromlastdayofweek = length - self.r_date.weekday()
+		if self.r_date.weekday() > 0:
+			result = (self.r_date.weekday() - timedelta(days=numberofdaysfromlastdayofweek).isoformat()
+			return result
+		else:
+			return self.r_date
+	
+	def lastdateofweek(self):
+		#returns a the last date of a given week determined by the date attribute passed in
+		length = len(returndate.weekdays)
+		numberofdaysfromlastdayofweek = length - self.r_date.weekday()
+		if self.r_date.weekday() < length:
+			result = (self.r_date.weekday() + timedelta(days=numberofdaysfromlastdayofweek).isoformat()
+			return result
+		else:
+			return self.r_date
+
   def __str__(self):
+		#returns a iso format string of the date object
     return str(self.r_date.isoformat())
