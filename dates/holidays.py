669028ed40213ff0c271aa6e9c74e0bb9968186e
Kino Mondesir <kino.mondesir@gmail.com>
2019-10-31 11:53:26 -0400

    Added more changes to holidays

--- dates/holidays.py
+++ dates/holidays.py
@@ -3,19 +3,19 @@ import datetime as dt
 
 class holidays(rd):
   
-  def __init__(self, title, r_date):
+  def __init__(self, title, year, r_date):
     
     super().__init__(r_date)
     self.title = title
     self.us = {
-      'New Years':dt.datetime.date(self.whatyear(),1,1),
-      'Martin Luther King':0,
-      'Memorial':0,
-      'Independence':dt.datetime.date(self.whatyear(),7,4),
-      'Labor': dt.datetime.strftime(self.firstdayofmonth(0)),
-      'Veterans':0,
-      'Thanksgiving':dt.datetime.strftime(self.lastdayofmonth(3)),
-      'Christmas':dt.datetime.date(self.las,12,25)
+      'New Years':dt.datetime.date(year,0,1),
+      'Martin Luther King': dt.datetime.strftime(self.lastdayofmonth(year, 0, 0) - dt.timedelta(days=7)),
+      'Memorial': dt.datetime.strftime(self.lastdayofmonth(year, 4, 0)),
+      'Independence':dt.datetime.date(year,6,4),
+      'Labor': dt.datetime.strftime(self.firstdayofmonth(year,8,0)),
+      'Veterans': dt.datetime.strftime(self.firstdayofmonth(year, 10, 0) + dt.timedelta(days=7)),
+      'Thanksgiving':dt.datetime.strftime(self.lastdayofmonth(year,10,3)),
+      'Christmas':dt.datetime.date(year,11,25)
     }
     
   def new_date(self):
