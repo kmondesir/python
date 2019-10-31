2fbf19f38a6bc66b1a218d7e736ec67ac300a6d8
Kino Mondesir <kino.mondesir@gmail.com>
2019-10-31 11:52:42 -0400

    Updated holidays and removed new_date argument

--- dates/holidays.py
+++ dates/holidays.py
@@ -1,23 +1,23 @@
-from python.dates.calculateddates import returndates as rd
+from calculateddates import returndates as rd
 import datetime as dt
 
 class holidays(rd):
   
-  def __init__(self, r_date, title):
+  def __init__(self, title, r_date):
     
     super().__init__(r_date)
     self.title = title
     self.us = {
-      'New Years':0,
+      'New Years':dt.datetime.date(self.whatyear(),1,1),
       'Martin Luther King':0,
       'Memorial':0,
-      'Independence':0,
-      'Labor':0,
+      'Independence':dt.datetime.date(self.whatyear(),7,4),
+      'Labor': dt.datetime.strftime(self.firstdayofmonth(0)),
       'Veterans':0,
-      'Thanksgiving':0,
-      'Christmas':0
+      'Thanksgiving':dt.datetime.strftime(self.lastdayofmonth(3)),
+      'Christmas':dt.datetime.date(self.las,12,25)
     }
     
-  def new_date(self, holiday):
-    result = self.us[holiday]
+  def new_date(self):
+    result = self.us[self.title]
     return result
