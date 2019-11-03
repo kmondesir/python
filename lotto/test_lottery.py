933cbd6c821075cfb407537a4b95703120c8b309
Kino Mondesir <kino.mondesir@outlook.com>
2019-11-03 14:42:43 -0500

    added comments to the test module

--- lotto/test_lottery.py
+++ lotto/test_lottery.py
@@ -4,33 +4,53 @@ from lottery import lotto
 class test_lotto(unittest.TestCase):
 
   def test_first_digit(self):
+    # test first digit against the control
     control = 6
     result = lotto('6 11 19 21 32').getFirstNumber()
-    msg = "{} is different from {}".format(result, control)
+    msg = "{} does not equal {}".format(result, control)
     self.assertEqual(result, control, msg)
 
   def test_last_digit(self):
+    # test last digit against the control
     control = 32
     result = lotto('6 11 19 21 32').getLastNumber()
-    msg = "{} is different from {}".format(result, control)
+    msg = "{} does not equal {}".format(result, control)
     self.assertEqual(result, control, msg)
 
   def test_total(self):
+    # test the sum against the control
     control = 89
     result = lotto('6 11 19 21 32').total()
-    msg = "{} is different from {}".format(result, control)
+    msg = "{} does not equal {}".format(result, control)
     self.assertEqual(result, control, msg)
 
   def test_average(self):
+    # test the average against the control
     control = 17.8
     result = lotto('6 11 19 21 32').average()
-    msg = "{} is different from {}".format(result, control)
+    msg = "{} does not equal {}".format(result, control)
     self.assertEqual(result, control, msg)
 
   def test_is_first_digit_single(self):
+    # test the first digit is single
     result = lotto('6 11 19 21 32').isFirstDigitSingle()
-    self.assertTrue(result)
+    msg = "result is not true"
+    self.assertTrue(result, msg)
 
-  def test_is_last_digit_single(self):
+  def test_is_first_digit_double(self):
+    # test the first digit is double
     result = lotto('10 11 19 21 32').isFirstDigitDouble()
-    self.assertTrue(result)
+    msg = "result is not true"
+    self.assertTrue(result, msg)
+
+  def test_mostly_odd(self):
+    # test if list is mostly odd
+    result = lotto('9 10 17 23 39').isMostlyOdd()
+    msg = "result is not true"
+    self.assertTrue(result, msg)
+
+  def test_first_odd(self):
+    # test if the first odd
+    result = lotto('9 10 17 23 39').firstOdd()
+    msg = "result is not true"
+    self.assertTrue(result, msg)
