2226f34a035f2b1a1b1a5c5994247376e71dd87b
Kino <kino.mondesir@gmail.com>
2019-11-07 08:36:36 -0500

    added space to comment delimiter

--- files/rename_files_and_directories.py
+++ files/rename_files_and_directories.py
@@ -21,18 +21,18 @@ def matchSpecialChars(value):
 
 def replaceSpecialCharsWithUnderScores(value):
   replacement = re.sub(chars,'_',value)
-  return replacement.strip() #remove spaces from the beginning and ending of string
+  return replacement.strip() # remove spaces from the beginning and ending of string
 
-for root, folders, items in os.walk(directory, topdown=True):
-  for folder in folders:
-    if matchSpecialChars(folder) == True:
-      current = os.path.join(root,folder)
-      new = os.path.join(root,replaceSpecialCharsWithUnderScores(folder))
-      print(current)
-      print(new)
-      print('')
-      c = c + 1
-  #os.rename(current, new)
+  for root, folders, items in os.walk(directory, topdown=True):
+    for folder in folders:
+      if matchSpecialChars(folder) == True:
+        current = os.path.join(root,folder)
+        new = os.path.join(root,replaceSpecialCharsWithUnderScores(folder))
+        print(current)
+        print(new)
+        print('')
+        c = c + 1
+  # os.rename(current, new)
   for item in items:
     if matchSpecialChars(item) == True:
       current = os.path.join(root,item)
@@ -41,7 +41,7 @@ for root, folders, items in os.walk(directory, topdown=True):
       print(new)
       print('')
       d = d + 1
-#os.rename(current,new)
+# os.rename(current,new)
 
 print('folders:' + str(c))
 print('files:' + str(d))
