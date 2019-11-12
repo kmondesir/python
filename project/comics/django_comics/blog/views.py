687e8e93d666716c826480d3e2af9960a69172cc
Kino <kino.mondesir@gmail.com>
2019-11-12 14:35:38 -0500

    Modified the url routes

--- project/comics/django_comics/blog/views.py
+++ project/comics/django_comics/blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
   return HttpResponse('<H1>Blog Home</H1>')
+
+def about(request):
+  return HttpResponse('<H1>Blog About</H1>')
