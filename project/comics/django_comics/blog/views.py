ab2114d6ed93322dee2c08a52bacdb32aa9a1924
Kino Mondesir <kino.mondesir@gmail.com>
2019-11-13 19:47:22 -0500

    added update

--- project/comics/django_comics/blog/views.py
+++ project/comics/django_comics/blog/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<H1>Blog Home</H1>')
+  return render(request, 'blog/home.html')
 
 def about(request):
-  return HttpResponse('<H1>Blog About</H1>')
+  return render(request, 'blog/about.html')
